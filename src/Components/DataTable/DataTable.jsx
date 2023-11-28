c90f2e9e987f81fbc6fa730cd56f4e8794af9653
Shruti Sharma <shruti1005sharma@gmail.com>
2023-11-28 19:50:15 +0530

    m1

--- src/Components/DataTable/DataTable.jsx
+++ src/Components/DataTable/DataTable.jsx
@@ -1,7 +1,5 @@
-// App.js
-
-import React, { useState, useEffect } from "react";
-import "./App.css"; // Import your CSS file for styling
+import React, { useState, useEffect } from 'react';
+import './DataTable.css'; // Import your CSS file for styling
 
 const DataTable = ({ data }) => {
   const [selectedRows, setSelectedRows] = useState([]);
@@ -20,11 +18,7 @@ const DataTable = ({ data }) => {
 
   const renderChildren = (children) => {
     return children.map((child) => (
-      <tr
-        key={child.id}
-        className={isRowSelected(child.id) ? "selected-row" : ""}
-        onClick={() => toggleRow(child.id)}
-      >
+      <tr key={child.id}>
         <td>{child.text1}</td>
         <td>{child.text2}</td>
       </tr>
@@ -35,15 +29,13 @@ const DataTable = ({ data }) => {
     return rows.map((row) => (
       <React.Fragment key={row.id}>
         <tr
-          className={isRowSelected(row.id) ? "selected-row" : ""}
+          className={isRowSelected(row.id) ? 'selected-row' : ''}
           onClick={() => toggleRow(row.id)}
         >
           <td>{row.text1}</td>
           <td>{row.text2}</td>
         </tr>
-        {row.children.length > 0 &&
-          isRowSelected(row.id) &&
-          renderChildren(row.children)}
+        {row.children.length > 0 && isRowSelected(row.id) && renderChildren(row.children)}
       </React.Fragment>
     ));
   };
@@ -56,7 +48,7 @@ const DataTable = ({ data }) => {
           <th>Text 2</th>
         </tr>
       </thead>
-      <tbody>{renderRows(data)}</tbody>
+      <tbody>{renderRows(data.rows)}</tbody>
     </table>
   );
 };
@@ -66,10 +58,10 @@ const App = () => {
 
   useEffect(() => {
     // Fetch data from the JSON file
-    fetch("https://shrutisharma25.github.io/Table__API/data.json")
+    fetch('/path/to/data.json')
       .then((response) => response.json())
-      .then((data) => setJsonData(data.rows))
-      .catch((error) => console.error("Error fetching data:", error));
+      .then((data) => setJsonData(data))
+      .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
   return (
